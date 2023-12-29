e7a5ea6518330132c46135bfe42b9870fa62f31b
Barış Atay <114981821+barisatay0@users.noreply.github.com>
2023-12-29 14:54:21 +0300

    Fix

--- Script.js
+++
@@ -1,9 +0,0 @@
-function myFunction(imgs) {
-    var expandImg = document.getElementById("ProfilePhoto");
-    var imgText = document.getElementById("imgtext");
-
-    expandImg.src = imgs.src;
-    imgText.textContent = imgs.alt;
-
-    expandImg.parentElement.style.display = "block";
-}
